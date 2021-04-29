53e38fc0da7816281839c34610247cc81d1ba294
Mackenzie <mackenziekynoch@gmail.com>
2021-04-28 21:11:14 -0600

    Created routes and methods in client to enable testing routes

--- client/app.jsx
+++ client/app.jsx
@@ -4,7 +4,54 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super();
+    this.state = {
+      currentBook: {}
+    };
   }
+
+  setParam() {
+    // let paramArr = document.location.search.substr(1).split('&');
+
+    // paramArr.push('bookId=1');
+    // let params = paramArr.join('&');
+
+    // document.location.search = params;
+    // document.location.search = "bookId=1";
+
+    const bookId = Math.floor(Math.random() * 100);
+    const bookTitle = encodeURIComponent('Of Journey My Lead And Begin');
+    const param = Math.random() < 0.5 ? `bookId=${bookId}` : `bookTitle=${bookTitle}`;
+    let newUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?${param}`;
+    window.history.pushState({ path: newUrl }, '', newUrl);
+  }
+
+  getPrice() {
+    console.log('==== getting price ====');
+    let params = document.location.search.substr(1).split('&');
+
+    params.forEach((item, i) => {
+      params[i] = item.split('=');
+    });
+
+    for (let param of params) {
+      if (param[0] === 'bookId' || param[0] === 'bookTitle') {
+        fetch(`http://localhost:3000/api/price/${param[1]}`)
+          .then(response => response.json()
+          .then(data => {
+            console.log('== book data ==>', data);
+            this.setState({
+              currentBook: data
+            });
+          }));
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.setParam();
+    this.getPrice();
+  }
+
   render() {
     return (
       <div id='app'>
