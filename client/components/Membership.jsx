cd976d0e66c48348d430fdc20bb768f2ffd7bdde
Mackenzie <mackenziekynoch@gmail.com>
2021-04-29 21:41:29 -0600

    Added styling to component

--- client/components/Membership.jsx
+++ client/components/Membership.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Membership = () => (
   <div id='membership'>
-    <div id='premiumPlus' className='priceTitle'>
+    <div id='premiumPlus' className='priceBody'>
       Included with <span id='premium'>premium</span><span id='plus'>plus</span>
     </div>
-    <div id='memberBenefites' className='priceBody'>
+    <div id='memberBenefits' className='priceBody'>
+      You'll have access to these benefits
       <ul>
         <li>1 audiobook of your choice.</li>
         <li>Stream or download thousands of included titles.</li>
