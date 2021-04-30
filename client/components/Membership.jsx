96b206ca341164dae3eb21ea6c72f68d3d35cb50
Mackenzie <mackenziekynoch@gmail.com>
2021-04-29 20:32:16 -0600

    Added missing text to Membership

--- client/components/Membership.jsx
+++ client/components/Membership.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 
 const Membership = () => (
   <div id='membership'>
-    <div id='premiumPlus' className='priceTitle'>
+    <div id='premiumPlus' className='priceBody'>
       Included with <span id='premium'>premium</span><span id='plus'>plus</span>
     </div>
-    <div id='memberBenefites' className='priceBody'>
+    <div id='memberBenefits' className='priceBody'>
+      You'll have access to these benefits
       <ul>
         <li>1 audiobook of your choice.</li>
         <li>Stream or download thousands of included titles.</li>
         <li>$14.95 a month after 30 day trial. Cancel anytime.</li>
       </ul>
     </div>
-    <button className='priceButton' id='memberEnroll'>Continue for free</button>
+    <button className='priceButton priceBody' id='memberEnroll'>Continue for free</button>
   </div>
 );
 
